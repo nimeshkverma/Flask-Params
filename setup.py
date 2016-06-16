1130f0d03162898fb25d8ecd750af55d11591b5d
nimeshkverma <nimesh.verma@craftsvilla.com>
2016-06-16 22:23:31 +0530

    Removed readme bug

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 from distutils.core import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
-
-
 setup(name='flask_params',
       packages=['flask_params'],
-      version='1.0.0',
+      version='1.0.1',
       description='Processes the Request params for Flask served as a Python library ',
-      long_description=long_description,
       author='Nimesh Kiran Verma, Rohit khatana',
       author_email='nimesh.aug11@gmail.com, rohitkhatana.khatana@gmail.com',
       url='https://github.com/nimeshkverma/Flask-Params',
-      download_url='https://github.com/nimeshkverma/Flask-Params/tarball/1.0.0',
+      download_url='https://github.com/nimeshkverma/Flask-Params/tarball/1.0.1',
       py_modules=['flask-params'],
       install_requires=['Flask', 'jsonschema'],
       keywords=['params', 'requests', 'post', 'get', 'API', 'REST'],
